d7ee0e1648862ee83c0cce8eaa04062576f3feed
Ali Usman <long_cafe@yahoo.com>
2024-06-05 13:53:51 +0700

    asiap

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// load in the environment vars
+require('dotenv').config({silent: true})
+
 const {
   makeWASocket,
   Browsers,
@@ -7,6 +10,7 @@ const {
 const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
+const logger = require('morgan')
 
 async function connectToWhatsApp() {
   const { state, saveCreds } = await useMultiFileAuthState("sessions/wa");
@@ -33,7 +37,7 @@ async function connectToWhatsApp() {
       // reconnect if not logged out
       if (shouldReconnect) {
         connectToWhatsApp();
-        console.log('ini sock', JSON.stringify(sock));
+        console.log("ini sock", JSON.stringify(sock));
       } else {
         console.log("connection closed");
         fs.rmdir(`sessions/wa`, { recursive: true }, (err) => {
@@ -52,38 +56,48 @@ async function connectToWhatsApp() {
 connectToWhatsApp();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//logger 
+app.use(logger('dev'))
+// urusan cors
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, API-Key')
+  next()
+})
+
 const infoMessage = (req, res) => {
-    try {
-        return res.status(200).json({
-            status: 'OK',
-            pesan: {
-                info: {
-                    url: 'kirim-pesan',
-                    method: 'get',
-                    keterangan: 'informasi menganai API WA'
-                },
-                kirim_pesan: {
-                    url: 'kirim-pesan',
-                    method: 'post',
-                    keterangan: 'harus menyertakan body nomor dan pesan'
-                }
-            }
-        })
-    } catch (error) {
-        return res.status(400).json({
-            'status': 'ERROR',
-            'pesan': error.message
-        })
-    }
-}
+  try {
+    return res.status(200).json({
+      status: "OK",
+      pesan: {
+        info: {
+          url: "kirim-pesan",
+          method: "get",
+          keterangan: "informasi menganai API WA",
+        },
+        kirim_pesan: {
+          url: "kirim-pesan",
+          method: "post",
+          keterangan: "harus menyertakan body nomor dan pesan",
+        },
+      },
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: "ERROR",
+      pesan: error.message,
+    });
+  }
+};
+
 
 const cekWa = async (req, res) => {
-    try {
-        let hp = req.body.nomor;
+  try {
+    let hp = req.body.nomor;
     let pesan = req.body.pesan;
     let delay = req.body.delay ? parseInt(req.body.delay) * 1000 : 2000;
     if (!hp || !pesan) {
@@ -99,18 +113,18 @@ const cekWa = async (req, res) => {
       jadi = hp;
     }
     const id = jadi + "@s.whatsapp.net"; // the WhatsApp ID
-        const cek = await sock.sendPresenceUpdate("available", id);
-        return res.status(200).json({
-            status: 'OK',
-            hasil: cek
-        })
-    } catch (error) {
-        return res.status(400).json({
-            status: 'ERROR',
-            hasil: error.message
-        })
-    }
-}
+    const cek = await sock.sendPresenceUpdate("available", id);
+    return res.status(200).json({
+      status: "OK",
+      hasil: cek,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: "ERROR",
+      hasil: error.message,
+    });
+  }
+};
 
 const sendMessage = async (req, res) => {
   try {
@@ -135,7 +149,7 @@ const sendMessage = async (req, res) => {
       status: "ok",
       message_id: send_message.key.id,
       penerima: id,
-      pesan: send_message,
+      pesan: send_message.message.extendedTextMessage.text,
     });
   } catch (error) {
     return res.status(400).json({
@@ -147,6 +161,18 @@ const sendMessage = async (req, res) => {
 
 app.get("/cek", cekWa);
 app.get("/kirim-pesan", infoMessage);
+
+// PROTECT ALL ROUTES THAT FOLLOW
+app.use((req, res, next) => {
+  const apiKey = req.get('API-Key')
+  const keys = String(process.env.API_KEY).split(',')
+  // if (!apiKey || apiKey !== process.env.API_KEY) {
+  if (!apiKey || !keys.includes(apiKey)) {
+    res.status(401).json({error: 'unauthorised'})
+  } else {
+    next()
+  }
+})
 app.post("/kirim-pesan", sendMessage);
 
 app.listen(port, () => {
